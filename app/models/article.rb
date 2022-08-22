f12299dae55f7dda0844c279a608a58f01a35864
Anais Augu <augu.anais@gmail.com>
2022-08-22 13:03:19 +0200

    simpleform installed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   validates :title, :content, :url, presence: true
+  validates :url, format: {with: /^(?:https:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/ }, message: "Format d'URL incorrect"}
 end
